4892357759a705b1005e3180535101afd5bc195f
Nazar.Hliviy <Nazar.Hliviy@payever.org>
2022-01-26 17:39:08 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MapChart from "./components/MapChart";
+import SideBar from "./components/SideBar";
+
+function App() {
+  return (
+    <div className="App">
+        <SideBar/>
+      <MapChart/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SideBar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './style.css';
+
+const SideBar = () => {
+    return (
+        <div>
+            <div className='wrapper'>
+                <div className='logo'>
+                    <a href="/general">
+                        LOGO
+                    </a>
+                </div>
+
+                <div className='list'>
+                    <ul className="menu">
+                        <li><a href="/general" className="menu-item">Trade</a></li>
+                        <li><a href="#" className="menu-item">Earn</a></li>
+                        <li><a href="#" className="menu-item">Win </a></li>
+                        <li><a href="/career" className="menu-item">NFT</a></li>
+                        <li><a href="#" className="menu-item">...</a></li>
+                    </ul>
+                </div>
+
+                <div className='wallet'>
+                    <a href="#" className="menu-item contact">Connect Wallet</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SideBar;

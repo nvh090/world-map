d8e072c1128747a0134128592783732e65bb95c7
nvh090 <nazar5182@icloud.com>
2022-01-26 17:59:08 +0200

    initial commit

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -14,15 +14,15 @@ const SideBar = () => {
                 <div className='list'>
                     <ul className="menu">
                         <li><a href="/general" className="menu-item">Trade</a></li>
-                        <li><a href="#" className="menu-item">Earn</a></li>
-                        <li><a href="#" className="menu-item">Win </a></li>
+                        <li><a href="/" className="menu-item">Earn</a></li>
+                        <li><a href="/" className="menu-item">Win </a></li>
                         <li><a href="/career" className="menu-item">NFT</a></li>
-                        <li><a href="#" className="menu-item">...</a></li>
+                        <li><a href="/" className="menu-item">...</a></li>
                     </ul>
                 </div>
 
                 <div className='wallet'>
-                    <a href="#" className="menu-item contact">Connect Wallet</a>
+                    <a href="/" className="menu-item contact">Connect Wallet</a>
                 </div>
             </div>
         </div>
